019b3f563fcc9d7b4e1805717f8221b6814e88fa
Nate McGregor <natecmcgregor@gmail.com>
2017-09-29 09:09:28 -0700

    create basic web layout

---
+++ app.rb
@@ -0,0 +1,11 @@
+require("pry")
+require("bundler/setup")
+Bundler.require(:default)
+
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get '/' do
+  @brands = Brand.all()
+  @stores = Store.all()
+  erb(:index)
+end
